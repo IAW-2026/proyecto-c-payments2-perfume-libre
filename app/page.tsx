f4c5c4e184b2ed4694ab636644dbd9fafa3efa6e
Integer <oyaagustin@gmail.com>
2026-05-12 12:43:44 -0300

    Setup Neon db connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
-import Image from "next/image";
+import { neon } from '@neondatabase/serverless';
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="text-6xl font-bold text-center sm:text-left">
-          Bienvenido a Payments App
-        </h1>
-      </main>
-    </div>
+
+async function create(formData: FormData) {
+    'use server';
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+    // Insert the comment from the form into the Postgres database
+    await sql`
+  INSERT INTO comments (comment)
+  VALUES (${comment})
+`;
+  }
+
+
+return (
+    <form action={create}>
+      <input type="text" placeholder="write a comment" name="comment" />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
